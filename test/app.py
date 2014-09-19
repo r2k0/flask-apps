2ab0fd8d80d78e13b528470fa7cd77d6ccaa1e2f
r2k0 <iheartcpp@gmail.com>
2014-09-19 08:06:44 -0700

    added 3 routes for int,float and dynamic

--- test/app.py
+++ test/app.py
@@ -1,4 +1,5 @@
 from flask import Flask
+""" learning and experimenting with Flask """
 
 # create an instance of the Flask class and assigned
 # it to the varialbe 'app'
@@ -13,6 +14,28 @@ app = Flask(__name__)
 @app.route("/test/<query>")
 def search(query):
 	return query
+
+#Flask converters
+# * <value> is treated as unicode
+# * <int:value> is treated as an integer
+# * <float:value> is treated as a floating point
+# * <path/of/some/sort> is treated as a path
+
+@app.route("/integer/<int:value>")
+def int_type(value):
+	print value + 1
+	return "correct"
+
+@app.route("/float/<float:value>")
+def float_type(value):
+	print value + 1
+	return "correct"
+
+# dynamic route that accpets slashes
+@app.route("/path/<path:value>")
+def path_type(value):
+	print value
+	return "correct"
 
 def test():
 	return "testing Flask!"
