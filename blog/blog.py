7138aa8f4d88e37d4df1bf59a28b89aebf43109c
r2k0 <iheartcpp@gmail.com>
2014-09-22 08:30:58 -0700

    template update

--- blog/blog.py
+++ blog/blog.py
@@ -16,5 +16,13 @@ def connect_db():
 """ db connection """
 	return sqlite3.connect(app.config['DATABASE'])
 
+@app.route('/')
+def login():
+    return render_template('login.html')
+
+@app.route('/main')
+def main():
+    return render_template('main.html')
+
 if __name__ == '__main__':
 	app.run(debug=True)
