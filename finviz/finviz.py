d87f9adbee84155459fe950b7a97fd1447b0275c
r2k0 <iheartcpp@gmail.com>
2014-09-23 23:26:33 -0700

    update

--- finviz/finviz.py
+++ finviz/finviz.py
@@ -5,8 +5,8 @@ from sqlite3 import dbapi2 as sqlite3
 from flask import Flask, request, session, g, redirect, \
         url_for, abort, render_template, flash, _app_ctx_stack
 
-#app * Flask(__name__)
-#app.config.from_object(__name__)
+app * Flask(__name__)
+app.config.from_object(__name__)
 
 #url = "http://finviz.com/export.ashx?v=111&f=sh_price_u1"
 url = "http://www.finviz.com/export.ashx?v=111&f=geo_usa,ind_exchangetradedfund,sh_curvol_o5000&o=-volume"
@@ -32,3 +32,6 @@ def get_series_value():
         except:
             print ""
     return zlist
+
+if __name__=='__main__':
+    app.run()
---
+++ finviz/getfvdata.py
@@ -0,0 +1,17 @@
+import urllib2
+import csv
+import pandas as pd
+
+def get_etfs(file):
+    url = "http://www.finviz.com/export.ashx?v=150&f=geo_usa,ind_exchangetradedfund,sh_curvol_o5000&o=-volume"
+    rawdata = urllib2.urlopen(url).readlines()
+    file = open(file,'wb')
+    for l in rawdata:
+        file.write(l)
+    file.close()
+
+def main():
+    get_etfs("etfdata.txt")
+
+if __name__ == '__main__':
+    main()
