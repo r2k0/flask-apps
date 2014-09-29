19de8a38ec17beed8778027afef17dd14adc93f7
r2k0 <iheartcpp@gmail.com>
2014-09-29 11:29:50 -0700

    update

--- finviz/app/app.py
+++ finviz/app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -7,9 +7,14 @@ def hello():
     return "finviz app"
 
 
-@app.route("/")
+@app.route("/",methods=['GET','POST'])
 def home():
-    return render_template('index.html')
+    if request.method == 'POST':
+        value_one = int(request.form['number-one'])
+        value_two = int(request.form['number-two'])
+        total = value_one + value_two
+        return render_template('index.html',string="TESTING!", value=total)
+    return render_template('index.html',string="TESTING!")
 
 if __name__ == '__main__':
     app.run(debug=True)
