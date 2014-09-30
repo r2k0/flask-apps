80d07d20fbbc495845e34b358270ffb1bb6b4358
r2k0 <iheartcpp@gmail.com>
2014-09-29 18:57:26 -0700

    update

--- mega-tut/app/views.py
+++ mega-tut/app/views.py
@@ -2,11 +2,26 @@
 the handlers that respond to requests from browsers or clients.
 Each view function is mapped to one or more request URLs.
 """
-
+from flask import render_template
 from app import app
 
 #the two decorators create mappings from URLs / and /index to this function
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'nickname': 'Okkar'}
+
+    posts = [
+            {
+                'author': {'nickname': 'Max'},
+                'body': 'Golden Gate Bridge!'
+            },
+            {
+                'author': {'nickname': 'Pan'},
+                'body': 'I want bacon!'
+            }
+            ]
+    return render_template('index.html',
+                            title='Home',
+                            user=user,
+                            posts=posts)
