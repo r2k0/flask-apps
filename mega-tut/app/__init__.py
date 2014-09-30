e6deb2ca0912330fa36c2d703e54a48ab36ab4c6
r2k0 <iheartcpp@gmail.com>
2014-09-30 14:11:23 -0700

    updates

--- mega-tut/app/__init__.py
+++ mega-tut/app/__init__.py
@@ -2,10 +2,17 @@
 creates the application object (of class Flask) and then
 imports the views module
 """
-
+import os
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask.ext.login import LoginManager
+from flask.ext.openid import OpenID
+from config import basedir
 
+lm = LoginManager()
+lm.init_app(app)
+lm.login_view = 'login'
+oid = OpenID(app, os.path.join(basedir, 'tmp'))
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
