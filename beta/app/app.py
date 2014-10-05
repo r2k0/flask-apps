4ee20e44fdc16e453783bb8af72985a6dbb765c9
r2k0 <iheartcpp@gmail.com>
2014-10-05 11:52:20 -0700

    updates

---
+++ beta/app/app.py
@@ -0,0 +1,24 @@
+import sqlite3
+from flask import Flask, request, session, g, redirect, url_for, \
+        abort, render_template, flash
+
+DATABASE = '/tmp/beta.db'
+DEBUG = True
+SECRET_KEY = 'development key'
+USERNAME = 'admin'
+PASSWORD = 'default'
+
+
+
+app = Flask(__name__)
+app.config.from_object(__name__)
+
+def connect_db():
+    return sqlite3.connect(app.config['DATABASE'])
+
+@app.route('/')
+def beta():
+    return 'welcome to beta!'
+
+if __name__ == '__main__':
+    app.run(debug=True)
