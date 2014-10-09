fb5dd59f108d186e4c2f52cb099a163861b80840
r2k0 <iheartcpp@gmail.com>
2014-10-09 14:31:44 -0700

    updates

--- beta/app/views.py
+++ beta/app/views.py
@@ -16,7 +16,7 @@ def connect_db():
 def beta():
     return render_template('index.html')
 
-@app.route('/properties/')
+@app.route('/forsale/')
 def properties():
     g.db = connect_db()
     cur = g.db.execute( 'select address, bed_rooms, \
@@ -25,6 +25,6 @@ def properties():
             bath_rooms=row[2], posted_date=row[3]) for row in cur.fetchall()]
     g.db.close()
     return render_template(
-            'properties.html',
+            'forsale.html',
             properties=properties
             )
